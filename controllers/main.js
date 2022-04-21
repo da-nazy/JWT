e289192174e3e00f0a71ae5f20db12ca97b71af7
da-nazy <danielani660@gmail.com>
2022-04-21 07:29:35 +0100

    recent update

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,6 @@
+// setup authentication so only the request with JWT can access the dashboard
+
+const jwt=require('jsonwebtoken')
 const CustomApiError=require('../errors/custom-error');
 const login=async(req,res)=>{
     const {username,password}=req.body;
@@ -7,14 +10,22 @@ const login=async(req,res)=>{
     if(!username||!password){
        throw new CustomApiError('Please provide email and password',400)
     }
-    res.send('Fake Login/Register/Signup Route')
+    //just for demo, normally provided by DB!!!
+    const id=new Date().getDate()
+
+    //try to keep payload small,better exerience for user
+    const token=jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
+
+    res.status(200).json({msg:'user created',token})
 }
 
 const dashboard=async(req,res)=>{
     const luckyNumber=Math.floor(Math.random()*100);
-    res.status(200).json(({msg:'Hello!, John Doe',secret:`Here is your authorized data, your lucky number ${luckyNumber} `}))
-}
-
+    res.status(200).json(({msg:`Hello, ${decoded.username}`,secret:`Here is your authorized data, your lucky number ${luckyNumber} `}))
+    }
+ 
 module.exports={
    login,dashboard 
 }
+
+//06:11:10
